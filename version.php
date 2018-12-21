498395eb7b40ceb4d81ef4c5d5d134e221904f90
David Watson <davidwatson@bpp.com>
2018-12-21 14:52:40 +0000

    Fixes issue #2, correcting capitalisation in language strings

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018092502;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018092503;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'format_tiles';    // Full name of the plugin (used for diagnostics).
