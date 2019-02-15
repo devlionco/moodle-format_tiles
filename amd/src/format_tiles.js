ec14fb50645b91a479b9b1cd76d4126c58a3b67c
David Watson <dw98@protonmail.com>
2019-02-15 12:30:28 +0000

    Fix header overlay height

--- amd/src/format_tiles.js
+++ amd/src/format_tiles.js
@@ -772,7 +772,7 @@ define(["jquery", "core/templates", "core/ajax", "format_tiles/browser_storage",
                         // Do not include for Moodle 3.5 or higher as not needed.
 
                         if (headerBar.height() !== undefined) {
-                            HEADER_BAR_HEIGHT = headerBar.height();
+                            HEADER_BAR_HEIGHT = headerBar.outerHeight();
                             headerOverlay = $("<div></div>")
                                 .addClass(ClassNames.HEADER_OVERLAY).attr("id", ClassNames.HEADER_OVERLAY)
                                 .css(CSS.DISPLAY, "none");
