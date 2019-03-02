cdc15e6978629024e821861f414db582e13de192
David Watson <dw98@protonmail.com>
2019-03-02 10:56:27 +0000

    Version 2019030100

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019013103;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2019030100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'format_tiles';    // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = "3.3.3.2";
+$plugin->release = "3.3.4";
